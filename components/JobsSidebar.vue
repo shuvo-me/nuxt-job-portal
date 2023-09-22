7fc1047473103f7a129b53f22678f140bec1b1e1
shuvo-me <shuvo.js.dev@gmail.com>
2023-09-22 18:30:35 +0600

    added devider in sidebar job list

--- components/JobsSidebar.vue
+++ components/JobsSidebar.vue
@@ -1,14 +1,12 @@
 <template>
   <aside class="bg-white max-w-[30rem] p-4 rounded-md">
-    <h5 class="text-[1.6rem] font-semibold border-b border-gray-100">
-      Related Jobs
-    </h5>
-    <ul class="space-y-5">
-      <li v-for="job in jobLists" :key="job.job_id">
-        <div class="flex items-start gap-x-3">
+    <h5 class="text-[1.6rem] font-semibold border-b">Related Jobs</h5>
+    <ul class="divide-y divide-gray-200">
+      <li v-for="job in jobLists" :key="job.job_id" class="py-[1.5rem]">
+        <div class="flex items-center gap-x-3">
           <div>
             <img
-              :src="job.employer_logo || CompanyPlaceHolderLogo"
+              :src="job?.employer_logo || CompanyPlaceHolderLogo"
               alt="job employer"
               class="w-[4.5rem] md:[5rem] lg:w-[5.5rem] xl:w-[6rem] rounded-md object-cover"
             />
